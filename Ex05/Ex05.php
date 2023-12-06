322116f6b72e8fb5910d45c17790a9d5146b4609
Arne <arne@loremipsum.ee>
2023-12-06 18:23:20 +0200

    Ex06

--- Ex05/Ex05.php
+++ Ex05/Ex05.php
@@ -8,7 +8,7 @@ class Ex05 extends Exercise
 {
     protected $maps = [];
     protected $seeds;
-    protected $min = 100000000000;
+    protected $min = INF;
 
     public function __construct()
     {
@@ -18,15 +18,47 @@ class Ex05 extends Exercise
 
     public function run()
     {
-        foreach ($this->seeds as $seed) {
-            $res = $seed;
+        // print_r($this->seeds);
 
-            foreach ($this->maps as $key => $map) {
-                $res = $this->getNext($res, $map, $key);
+        // part1
+        // foreach ($this->seeds as $seed) {
+        //     $res = $seed;
+
+        //     foreach ($this->maps as $key => $map) {
+        //         $res = $this->getNext($res, $map, $key);
+        //     }
+
+        //     if ($res < $this->min) {
+        //         $this->min = $res;
+        //     }
+        // }
+
+        foreach ($this->seeds as $index => $seed) {
+            if ($index % 2 == 0) {
+                continue;
             }
+            $start = $this->seeds[$index - 1];
+            $range = $seed;
 
-            if ($res < $this->min) {
-                $this->min = $res;
+            // print_r('Pair ' . $index - 1 . ' and ' . $index . PHP_EOL);
+            // print_r('Length: ' . ($start + $range) . PHP_EOL);
+
+            for ($i=$start; $i < $start + $range; $i++) { 
+                $res = $i;
+                if ($i % 100000 == 0) {
+                    print_r('Seeds: ' . $i . PHP_EOL);
+
+                }
+
+                foreach ($this->maps as $key => $map) {
+                    $res = $this->getNext($res, $map, $key);
+                }
+
+
+                // print_r($res . PHP_EOL);
+                if ($res < $this->min) {
+                    $this->min = $res;
+                }
             }
         }
 
--- Ex06/Ex06.php
+++ Ex06/Ex06.php
@@ -16,11 +16,69 @@ class Ex06 extends Exercise
 
     public function run()
     {
-        print_r('ok');
+        $end = 1;
+        foreach ($this->map['Time:'] as $key => $time) {
+            $counter = 0;
+            $toBeat = $this->map['Distance:'][$key];
+
+            $high = $this->getMiddlePart($time);
+
+            if (count($high) == 2) {
+                for ($i=$high[1]; $i <= $time; $i++) {
+                    $speed = $time - $i;
+                    $res = $speed * $i;
+
+                    if ($res > $toBeat) {
+                        $counter += 2;
+                    }
+                }
+            } else {
+                for ($i=$high[0]; $i <= $time; $i++) {
+                    $speed = $time - $i;
+                    $res = $speed * $i;
+
+                    if ($res > $toBeat) {
+                        $counter += 2;
+                    }
+                }
+                $counter -= 1;
+            }
+
+            $end *= $counter;
+        }
+
+        print_r($end);
+    }
+
+    public function getMiddlePart($number)
+    {
+        if ($number % 2 != 0) {
+            return [($number - 1) / 2, (($number - 1) / 2) + 1];
+        } else {
+            return [$number / 2];
+        }
     }
 
     public function parseInput($arr)
     {
-        $this->map = $arr;
+        foreach ($arr as $row) {
+            // part 1
+            // $clean = preg_replace('/\s+/', ' ', $row);
+            // $exploded = explode(' ', $clean);
+
+            // part 2
+            $clean = preg_replace('/\s+/', '', $row);
+            $exploded = explode(':', $clean);
+
+            print_r($exploded);
+            $this->map[$exploded[0] . ':'] = [
+                $exploded[1],
+                
+                // part 1
+                // $exploded[2],
+                // $exploded[3],
+                // $exploded[4],
+            ];
+        }
     }
 }
