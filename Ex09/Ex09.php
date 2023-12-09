2de2c8429ec60f760f4b4ef0c36018d3640af1c1
Arne <arne@loremipsum.ee>
2023-12-09 20:31:03 +0200

    Ex09

--- Ex09/Ex09.php
+++ Ex09/Ex09.php
@@ -1,12 +1,13 @@
 <?php
 
-namespace Aoc2023\Ex04;
+namespace Aoc2023\Ex09;
 
 use Aoc2023\Main\Exercise;
 
-class Ex04 extends Exercise
+class Ex09 extends Exercise
 {
-    protected $map;
+    protected $lines;
+    protected $level;
 
     public function __construct()
     {
@@ -16,11 +17,48 @@ class Ex04 extends Exercise
 
     public function run()
     {
-        print_r('ok');
+        $right = 0;
+        $left = 0;
+
+        foreach ($this->lines as $line) {
+            $toAdd = 0;
+            $toSub = 0;
+            $newLine = $line;
+            $multiplier = 1;
+
+            while (!empty($newLine)) {
+                $newLine = $this->getNewLine($newLine);
+                $multiplier *= -1;
+                $toAdd += end($newLine);
+                $toSub += reset($newLine) * $multiplier;
+            }
+
+            $right += (end($line) + $toAdd);
+            $left += (reset($line) + $toSub);
+        }
+
+        print_r($left);
+    }
+
+    public function getNewLine($arr)
+    {
+        $res = [];
+        $zeroCounter = 0;
+        for ($i=0; $i < count($arr) - 1; $i++) {
+            $sum = $arr[$i + 1] - $arr [$i];
+            if ($sum == 0) {
+                $zeroCounter ++;
+            }
+            $res[] = $sum;
+        }
+
+        return $zeroCounter == count($res) ? [] : $res;
     }
 
     public function parseInput($arr)
     {
-        $this->map = $arr;
+        foreach ($arr as $row) {
+            $this->lines[] = explode(' ', $row);
+        }
     }
 }
