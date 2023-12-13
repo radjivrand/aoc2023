6f280fdeb9b41ccd5416be585762abb65af90afd
Arne <arne@loremipsum.ee>
2023-12-13 21:21:07 +0200

    Ex13, part II

--- Ex13/Ex13.php
+++ Ex13/Ex13.php
@@ -4,17 +4,89 @@ namespace Aoc2023\Ex13;
 
 use Aoc2023\Main\Exercise;
 
+// too high 28834
+
 class Ex13 extends Exercise
 {
     protected $maps;
+    protected $lines = [];
 
     public function __construct()
     {
         $this->parseInput($this->getFileContents());
-        $this->run();
+        // $this->runPartOne();
+        print_r($this->runPartTwo());
     }
 
-    public function run()
+    public function runPartTwo()
+    {
+        foreach ($this->maps as $mp) {
+            foreach ($mp as $key => $line) {
+                if (isset($mp[$key + 1])) {
+                    $res = $this->isAlmostMirror($key, $key + 1, $mp) * 100;
+
+                    if ($res > 0) {
+                        $sum += $res;
+                        continue 2;
+                    }
+                }
+            }
+
+            $ver = $this->rotate($mp); 
+            
+            foreach ($ver as $key => $line) {
+                if (isset($ver[$key + 1])) {
+                    $res = $this->isAlmostMirror($key, $key + 1, $ver);
+
+                    if ($res > 0) {
+                        $sum += $res;
+                        continue 2;
+                    }
+                }
+            }
+        }
+
+        return $sum;
+    }
+
+    public function isAlmostMirror($a, $b, $map)
+    {
+        $len = count($map);
+        $initialA = $a;
+        $mistakes = 0;
+
+        for ($i=0; $i < $len; $i++) {
+            if (!isset($map[$a - $i]) || !isset($map[$b + $i])) {
+                if ($mistakes == 1) {
+                    return $initialA + 1;
+                } else {
+                    return 0;
+                }
+            }
+
+            $mistakes += $this->aboutEquals($map[$a - $i], $map[$b + $i]);
+
+            if ($mistakes > 1) {
+                return 0;
+            }
+        }
+
+        return 0;
+    }
+
+    public function aboutEquals($a, $b)
+    {
+        $b = str_split($b);
+        $score = 0;
+
+        foreach (str_split($a) as $key => $char) {
+            $score += $char == $b[$key] ? 0 : 1;
+        }
+
+        return $score;
+    }
+
+    public function runPartOne()
     {
         $sum = 0;
 
@@ -53,7 +125,7 @@ class Ex13 extends Exercise
         $len = count($map);
         $initialA = $a;
 
-        for ($i=1; $i < $len; $i++) {
+        for ($i=0; $i < $len; $i++) {
             if (!isset($map[$a - $i]) || !isset($map[$b + $i])) {
                 return $initialA + 1;
             }
