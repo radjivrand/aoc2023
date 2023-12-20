53b3eae3e13cec192044357627166db263b0d25e
Arne <arne@loremipsum.ee>
2023-12-20 18:41:16 +0200

    Ex19, part I

--- Ex12/Ex12.php
+++ Ex12/Ex12.php
@@ -4,26 +4,30 @@ namespace Aoc2023\Ex12;
 
 use Aoc2023\Main\Exercise;
 
+// part 2: 1074655979884 too low
+
 class Ex12 extends Exercise
 {
     protected $lines;
     protected $scores;
     protected $secondScores;
+    protected $masterPerms = [];
 
     public function __construct()
     {
         $this->parseInput($this->getFileContents());
 
         $this->run();
-        // $this->runPartTwo();
+        $this->runPartTwo();
     }
 
-    public function possibilities($groups, $items)
+    public function possibilities($groups, $items, $level = 0) 
     {
         if ($groups == 2) {
             $res = [];
 
-            for ($i= 0; $i <= $items; $i++) {
+            $start = $level > 0 ? 1 : 0;
+            for ($i= $start; $i <= $items; $i++) {
                 $pair = [$i, $items - $i];
                 $res[] = $pair;
             }
@@ -33,8 +37,10 @@ class Ex12 extends Exercise
 
         $out = [];
 
-        for ($i=0; $i <= $items; $i++) { 
-            $res = $this->possibilities($groups - 1, $items - $i);
+        $start = $level > 0 ? 1 : 0;
+        $level = $level + 1;
+        for ($i=$start; $i <= $items; $i++) { 
+            $res = $this->possibilities($groups - 1, $items - $i, $level);
 
             foreach ($res as &$value) {
                 array_unshift($value, $i);
@@ -49,8 +55,11 @@ class Ex12 extends Exercise
     public function runPartTwo()
     {
         $res = 0;
+        $endRes = 0;
 
         foreach ($this->lines as $key => $ln) {
+            // $pieces = $ln['pcs'];
+            // $line = $ln['line'];
             $pieces = array_merge($ln['pcs'], $ln['pcs']);
             $line = $ln['line'] . '?' . $ln['line'];
 
@@ -59,10 +68,21 @@ class Ex12 extends Exercise
             $localScore = 0;
 
             print_r('_________________________' . PHP_EOL);
-            print_r('About to create perms for ' . (count($pieces) + 1) . ' items and ' . ($data['length'] - array_sum($pieces)) . ' spaces' . PHP_EOL);
-            $perms = $this->possibilities(count($pieces) + 1, ($data['length'] - array_sum($pieces)));
-            print_r('result count: ' . count($perms) . PHP_EOL);
+            // print_r([$line]);
+            // print_r($data);
+            // print_r($pieces);
+            // print_r('About to create perms for ' . (count($pieces) + 1) . ' items and ' . ($data['length'] - array_sum($pieces)) . ' spaces' . PHP_EOL);
 
+            $groups = count($pieces) + 1;
+            $items = $data['length'] - array_sum($pieces);
+
+            if (!isset($this->masterPerms[$groups . ':' . $items])) {
+                $perms = $this->possibilities($groups, $items);
+                $this->masterPerms[$groups . ':' . $items] = $perms;
+            } else {
+                $perms = $this->masterPerms[$groups . ':' . $items];
+            }
+                
             $perms = array_filter($perms, function($item) {
                 $length = count($item);
                 for ($i=1; $i < $length - 1; $i++) {
@@ -87,19 +107,17 @@ class Ex12 extends Exercise
                 }
             }
 
-            print_r('Line no '. $key .': local score for 2nd pass: ' . $localScore . PHP_EOL);
-
             $res += $localScore;
             $this->secondScores[] = $localScore;
+            $div = $localScore / $this->scores[$key];
+
+            print_r('Line no '. $key + 1 .': local score for 2nd pass: ' . $localScore . PHP_EOL);
+            print_r('Divide '. $localScore  . ' by ' .  $this->scores[$key] .' = ' . $div . PHP_EOL);
+
+            $endRes += $this->scores[$key] * $div * $div * $div * $div;
         }
 
-        $endRes = 0;
-
-        foreach ($this->scores as $key => $value) {
-            $multiplier = $this->secondScores[$key] / $value;
-            $endRes += $value * ($multiplier ** 4);
-        }
-
+        // print_r($res);
         print_r($endRes);
     }
 
